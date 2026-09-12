cb8ba82a6f2a16dd9bd480366d0a222a954957b4
azimxonmeliyev9-maker <azimxonmeliyev9@gmail.com>
2026-09-12 11:57:19 +0300

    fix: update launcher to reliably target Chrome/Edge executables

--- Ishga_Tushirish.bat
+++ Ishga_Tushirish.bat
@@ -5,5 +5,23 @@ echo     AZIMXON.HUB - Raqamli Marketplace Platformasi
 echo ===================================================
 echo.
 echo Veb-ilova brauzerda ochilmoqda...
-start "" "%~dp0index.html"
-exit
+
+set "FILE_PATH=%~dp0index.html"
+
+if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
+    start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" "%FILE_PATH%"
+    exit /b
+)
+
+if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
+    start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "%FILE_PATH%"
+    exit /b
+)
+
+if exist "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" (
+    start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "%FILE_PATH%"
+    exit /b
+)
+
+start "" "%FILE_PATH%"
+exit /b
